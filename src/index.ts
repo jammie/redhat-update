aa1faaafcd3cb3c0164e2771f049cc4035e4034d
alamsyah ahad <alamsyahahad@gmail.com>
2020-02-15 15:53:24 +0700

    Fix criteria attributes error and Doing Code Refactor

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 var request = require('request');
 var XmlStream = require('xml-stream');
 var fs = require("fs");
-
-
 export default class Main {
   private redhatUpdateUrl = "http://www.redhat.com/security/data/oval/com.redhat.rhsa-all.xml";
 
@@ -12,7 +10,7 @@ export default class Main {
     }
   }
 
-  getUrl = () => {
+  run = () => {
     console.log(this.redhatUpdateUrl);
     const $this = this;
     const url = this.redhatUpdateUrl;
@@ -28,89 +26,65 @@ export default class Main {
           console.log( "download done..." );
         });
       }
-      $this.readXML(outputPath);
-    } catch(err) {
-      console.error(err)
+      $this.convertToJSON ( outputPath );
+    } catch ( err)  {
+      console.error( err )
     }
   }
-
-  checkCriteria = (criteria) => {
-    var data = {
-
-    };
-    return data;
-  }
-
-  readXML = ( outputPath =  "./redhat-update.xml" ) => {
+  convertToJSON = ( outputPath =  "./redhat-update.xml" ) => {
     const $this = this;
     const jsonParsedPath ='./redhat-update-output.json';
     try {
-      if ( fs.existsSync( jsonParsedPath) ) {
+      if ( fs.existsSync( jsonParsedPath )) {
         fs.unlinkSync( jsonParsedPath );
       };
 
-      var readStream = fs.createReadStream(outputPath);
-      var writeStream = fs.createWriteStream('./redhat-update-output.json');
-      var xml = new XmlStream(readStream);
-      var obj = {};
-      //xml.preserve('definitions');
+      const readStream = fs.createReadStream( outputPath );
+      const writeStream = fs.createWriteStream( './redhat-update-output.json' );
+      const xml = new XmlStream( readStream );
 
-      var definitions = []
-      xml.on('endElement: definition', function(item) {
-        console.log(item);
-        var definition = {};
+      let obj = {};
+      let definitions = []
+
+      xml.on( 'endElement: definition', function ( item ) {
+        let definition = {};
         definition["title"] = item["metadata"]["title"];
         definition["fixed_cve"] = item["metadata"]["advisory"]["cfe"];
         definition["severity"] = item["metadata"]["advisory"]["severity"];
         definition["affected_cpe"] = item["metadata"]["advisory"]["affected_cpe_list"];
-        /*
-        var criteriaData = {
-          "or": [
+        let criteriaData = [
             {
               "AND": [
-                [item["criteria"]["criterion"]["$"]["version"], item["criteria"]["criterion"]["$"]["id"], item["criteria"]["criterion"]["$"]["class"]]
+                [item["criteria"]["criterion"]["$"]["test_ref"], item["criteria"]["$"]["comment"]],
+                [item["criteria"]["criteria"]["criterion"]["$"]["test_ref"], item["criteria"]["criteria"]["criterion"]["$"]["comment"]]
               ]
             },
-            {
-              "AND": [
-                [item["criteria"]["criteria"]["criterion"]["$"]["version"], item["criteria"]["criteria"]["criterion"]["$"]["id"], item["criteria"]["criteria"]["criterion"]["$"]["class"]]
-              ]
-            },
-            {
-              "AND": [
-                [item["criteria"]["criteria"]["criteria"]["criterion"]["$"]["version"], item["criteria"]["criteria"]["criterion"]["$"]["id"], item["criteria"]["criteria"]["criteria"]["criterion"]["$"]["class"]]
-              ]
-            }
           ]
-        };
-        */
-
         definition["criteria"] = [
           {
             "or": [
               {
                 "and": [
-                  ["version", "id", "class"]
-                ]
+                  ["test_ref", "comment"]
+                ],
+                "or" : criteriaData
               }
             ]
           }
         ];
-        //definition["criteria"]["or"]["and"].push(criteriaData);
-        definitions.push(definition);
-        writeStream.write(JSON.stringify({"advisory": definitions}));
+        definitions.push( definition );
+        writeStream.write( JSON.stringify( { "advisory": definitions } ));
       });
-      xml.on("end", function() {
+      xml.on( "end", function () {
       	writeStream.end();
-      	console.log("finished");
+      	console.log( "finished" );
       });
 
-    } catch(err) {
-      console.error(err)
+    } catch ( err ) {
+      console.error( err )
     }
-
   }
 }
 
-let x = new Main();
-x.getUrl();
+const main = new Main();
+main.run();
